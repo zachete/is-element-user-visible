c2ebbaae09af49aaa3aadd285d97a31eda03fbae
Aleha <emelianenko@elfsight.com>
2018-10-01 12:05:47 +0300

    Input data format changed. Custom parent support

--- index.js
+++ index.js
@@ -1,20 +1,67 @@
-module.exports = function(element, percentage) {
-    let el = element;
+module.exports = function(inputData) {
+    var config = {};
 
-    if (!percentage) {
-        percentage = 100;
+    if (inputData instanceof Element) {
+        config = {
+            element: inputData,
+            percentage: 100
+        }
+    } else {
+        config = {
+            element: inputData.element || null,
+            parent: inputData.parent || null,
+            percentage: inputData.percentage || null
+        };
     }
 
-    var visibleInsideViewport = function(element) {
-        var rect = element.getBoundingClientRect();
-        var widthFragment = rect.width / 100 * (100 - percentage);
-        var heightFragment = rect.height / 100 * (100 - percentage);
+    var visibleInsideViewport = function() {
+        var rect = config.element.getBoundingClientRect();
+        var widthFragment = rect.width / 100 * (100 - config.percentage);
+        var heightFragment = rect.height / 100 * (100 - config.percentage);
 
         var rules = [
             rect.top >= (0 - heightFragment),
             rect.left >= (0 - widthFragment),
             rect.bottom <= window.innerHeight + heightFragment,
             rect.right <= window.innerWidth + widthFragment
+        ];
+
+        return rules.every(function(item) {return item});
+    };
+
+    var visibleInsideScrollableContent = function() {
+        var getScrollParent = function(element, includeHidden) {
+            var style = getComputedStyle(element);
+            var excludeStaticParent = style.position === 'absolute';
+            var overflowRegex = includeHidden ? /(auto|scroll|hidden)/ : /(auto|scroll)/;
+
+            if (style.position === 'fixed') return document.body;
+
+            for (var parent = element; (parent = parent.parentElement);) {
+                style = getComputedStyle(parent);
+
+                if (excludeStaticParent && style.position === 'static') {
+                    continue;
+                }
+
+                if (overflowRegex.test(style.overflow + style.overflowY + style.overflowX))
+                    return parent;
+            }
+
+            return element.parentNode || document.body;
+        };
+
+        var scrollableParent = config.parent || getScrollParent(config.element, true);
+        var scrollableParentRect = scrollableParent.getBoundingClientRect();
+        var elementRect = config.element.getBoundingClientRect();
+        var widthFragment = elementRect.width / 100 * (100 - config.percentage);
+        var heightFragment = elementRect.height / 100 * (100 - config.percentage);
+
+        var rules = [
+            elementRect.y >= scrollableParentRect.y - heightFragment,
+            elementRect.y + elementRect.height <= (scrollableParentRect.y + scrollableParentRect.height) + heightFragment,
+            elementRect.x >= scrollableParentRect.x - widthFragment,
+            elementRect.x + elementRect.width <= (scrollableParentRect.x + scrollableParentRect.width) + widthFragment,
         ];
 
         return rules.every(function(item) {return item});
@@ -39,43 +86,7 @@ module.exports = function(element, percentage) {
         return false;
     };
 
-    var visibleInsideScrollableContent = function(element) {
-        var getScrollParent = function(element, includeHidden) {
-            var style = getComputedStyle(element);
-            var excludeStaticParent = style.position === 'absolute';
-            var overflowRegex = includeHidden ? /(auto|scroll|hidden)/ : /(auto|scroll)/;
-
-            if (style.position === 'fixed') return document.body;
-
-            for (var parent = element; (parent = parent.parentElement);) {
-                style = getComputedStyle(parent);
-
-                if (excludeStaticParent && style.position === 'static') {
-                    continue;
-                }
-
-                if (overflowRegex.test(style.overflow + style.overflowY + style.overflowX))
-                    return parent;
-            }
-
-            return element.parentNode || document.body;
-        };
-
-        var scrollableParent = getScrollParent(element);
-        var scrollableParentRect = scrollableParent.getBoundingClientRect();
-        var elementRect = element.getBoundingClientRect();
-        var widthFragment = elementRect.width / 100 * (100 - percentage);
-        var heightFragment = elementRect.height / 100 * (100 - percentage);
-
-        var rules = [
-            elementRect.y >= scrollableParentRect.y - heightFragment,
-            elementRect.y + elementRect.height <= (scrollableParentRect.y + scrollableParentRect.height) + heightFragment,
-            elementRect.x >= scrollableParentRect.x - widthFragment,
-            elementRect.x + elementRect.width <= (scrollableParentRect.x + scrollableParentRect.width) + widthFragment,
-        ];
-
-        return rules.every(function(item) {return item});
-    };
-
-    return visibleInsideViewport(el) && visibleByStyles(el) && visibleInsideScrollableContent(el);
+    return visibleInsideViewport()
+        && visibleInsideScrollableContent()
+        && visibleByStyles(config.element);
 };
